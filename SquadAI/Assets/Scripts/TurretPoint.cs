5d6f0483ad38102640f12d855aaeb03c88ec6d0d
Harry <harry3.newton@live.uwe.ac.uk>
2021-02-08 16:02:45 +0000

    Turret Selection

---
+++ SquadAI/Assets/Scripts/TurretPoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TurretPoint : MonoBehaviour
+{
+    [Header("Turret Point Setup")]
+    public Canvas buyCanvas;
+    public GameObject ballista;
+
+    private bool purchasedTurret;
+
+    private void Awake()
+    {
+        buyCanvas.enabled = false;
+
+        ballista.SetActive(false);
+    }
+
+    public void BuyBallista()
+    {
+        ballista.SetActive(true);
+
+        purchasedTurret = true;
+        
+        buyCanvas.enabled = false;
+    }
+
+    private void OnMouseOver()
+    {
+        if (!purchasedTurret)
+        {
+            buyCanvas.enabled = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!purchasedTurret)
+        {
+            buyCanvas.enabled = false;
+        }
+    }
+}
---
+++ SquadAI/Assets/Scripts/TurretPurchaseCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretPurchaseCanvas : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform); // Keeps the healthbar rotated towards the camera
+        transform.Rotate(0, 180, 0);
+    }
+}
