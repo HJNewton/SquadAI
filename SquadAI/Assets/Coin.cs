f884484d7a0cdccd5685eacd37b5c99bbf19825b
Harry <harry3.newton@live.uwe.ac.uk>
2021-02-10 16:07:59 +0000

    Working on coins

---
+++ SquadAI/Assets/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int coinsToGive = 10;
+    public Transform coin;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        coin.transform.Rotate(Vector3.one * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Squad Member"))
+        {
+            GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManagerScript>().coinsTotal += coinsToGive;
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SquadAI/Assets/Scripts/EnemyDrops.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDrops : MonoBehaviour
+{
+    [Header("Drops Setup")]
+    public GameObject coinPrefab;
+
+    void OnDestroy()
+    {
+        Instantiate(coinPrefab, transform.position, transform.rotation);
+    }
+}
